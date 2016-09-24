e5e298b86ed0dfdce306a92707ad23f624a54649
krl123 <krl123@gmail.com>
2016-09-24 19:54:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/page1', function (req, res) {
+  res.send("Article 1 here ");
+});
+
+app.get('/page2', function (req, res) {
+  res.send("Article 2 here ");
+});
+
+app.get('/page3', function (req, res) {
+  res.send("Article 3 here ");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
